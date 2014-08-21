1c21b1e16c5e9460154ac4ce0db6f60b1625401a
AlexHRG <hirurg_03@ukr.net>
2014-08-21 16:53:02 +0300

    show picture on hamster page, added scrolling

---
+++ src/com/example/unrealmojo/test/GetImage.java
@@ -0,0 +1,30 @@
+package com.example.unrealmojo.test;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+public class GetImage {
+	
+	public static Bitmap getRemoteImage(String imageUrl) {
+	    try {
+	    	URL url = new URL(imageUrl);
+	        final URLConnection conn = url.openConnection();
+	        conn.connect();
+	        final BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
+	        final Bitmap bm = BitmapFactory.decodeStream(bis);
+	        bis.close();
+
+	        return bm;
+	    } catch (IOException e) {
+	        Log.d("Your tag here", "Connection error");
+	    }
+	    return null;
+	}
+
+}
