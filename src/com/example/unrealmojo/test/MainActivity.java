0a61a25d6f6aebc7d2d7264a8d593a391ed9b073
AlexHRG <hirurg_03@ukr.net>
2014-08-29 13:18:04 +0300

    fixed main activity architectory

--- src/com/example/unrealmojo/test/MainActivity.java
+++ src/com/example/unrealmojo/test/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,6 +36,7 @@ public class MainActivity extends Activity {
 	private static String myFolderName = "/UnrealMojo";
 	private static String myFileName = "mojo";
 	private ProgressDialog pDialog;
+	GetHamsters getHamsters;
 	ArrayList<Map<String, String>> hamster_list = new ArrayList<Map<String, String>>();
 	ListView listView;
 
@@ -45,37 +47,76 @@ public class MainActivity extends Activity {
 
 		listView = (ListView) findViewById(android.R.id.list);
 		
-		new GetHamsters().execute();
+		getHamsters = new GetHamsters();
+		
+		try {
+			hamster_list = getHamsters.execute().get();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
+				hamster_list, R.layout.list, new String[] { TAG_TITLE,
+						TAG_DESCRIPTION, TAG_IMAGEPATH }, new int[] {
+						R.id.listTitle, R.id.listDescription,
+						R.id.listImage });
+
+		listView.setAdapter(adapter);
+		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+		listView.setOnItemClickListener(new ListView.OnItemClickListener() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public void onItemClick(AdapterView<?> parent,
+					View itemClicked, int position, long id) {
+
+				Intent intent = new Intent(MainActivity.this,
+						HamsterActivity.class);
+				Map<String, String> currentItem = (Map<String, String>) parent.getItemAtPosition(position);
+
+				intent.putExtra(TAG_TITLE, currentItem.get(TAG_TITLE));
+				intent.putExtra(TAG_DESCRIPTION,currentItem.get(TAG_DESCRIPTION));
+				intent.putExtra(TAG_IMAGEPATH, currentItem.get(TAG_IMAGEPATH));
+				
+				startActivity(intent);
+
+			}
+
+		});
 
 	}
 	
 //	protected void onSaveInstanceState(Bundle outState) {
 //	    super.onSaveInstanceState(outState);
-//	    outState.putSerializable("hamsters", hamster_list);
+//	    outState.putParcelableArrayList("hamsters", (ArrayList<    ? extends Parcelable>) hamster_list);
 //	    Log.d(LOG_TAG, "onSaveInstanceState");
 //	  }
 //	
 //	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 //	    super.onRestoreInstanceState(savedInstanceState);
-//	    hamster_list = (ArrayList<Map<String, String>>) savedInstanceState.getSerializable("hamsters");
+//	    hamster_list = (ArrayList<Map<String, String>>) savedInstanceState.get("hamsters");
 //	    Log.d(LOG_TAG, "onRestoreInstanceState");
 //	  }
 
 
-	private class GetHamsters extends AsyncTask<Void, Void, Void> {
+	private class GetHamsters extends AsyncTask<Void, Void, ArrayList<Map<String, String>>> {
 
 		@Override
 		protected void onPreExecute() {
-			super.onPreExecute();
+			//super.onPreExecute();
 			pDialog = new ProgressDialog(MainActivity.this);
 			pDialog.setMessage("Please wait...");
 			pDialog.setCancelable(true);
-			pDialog.show();
+			if (!pDialog.isShowing()){
+				pDialog.show();
+			}
 
 		}
 
 		@Override
-		protected Void doInBackground(Void... arg0) {
+		protected ArrayList<Map<String, String>> doInBackground(Void... arg0) {
 
 			ServiceHandler sh = new ServiceHandler();
 
@@ -146,6 +187,7 @@ public class MainActivity extends Activity {
 								.add((HashMap<String, String>) hamster_data);
 						
 						Collections.sort(hamster_list, new HamsterComparator());
+						
 					}
 
 				} catch (JSONException e) {
@@ -155,44 +197,15 @@ public class MainActivity extends Activity {
 				Log.d(LOG_TAG, "Couldn't get any data from server");
 			}
 
-			return null;
+			return hamster_list;
 		}
 
-		@Override
-		protected void onPostExecute(Void result) {
+		protected void onPostExecute(ArrayList<Map<String, String>> result) {
 			super.onPostExecute(result);
 
 			if (pDialog.isShowing())
 				pDialog.dismiss();
 
-			SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
-					hamster_list, R.layout.list, new String[] { TAG_TITLE,
-							TAG_DESCRIPTION, TAG_IMAGEPATH }, new int[] {
-							R.id.listTitle, R.id.listDescription,
-							R.id.listImage });
-
-			listView.setAdapter(adapter);
-			listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-
-			listView.setOnItemClickListener(new ListView.OnItemClickListener() {
-				@SuppressWarnings("unchecked")
-				@Override
-				public void onItemClick(AdapterView<?> parent,
-						View itemClicked, int position, long id) {
-
-					Intent intent = new Intent(MainActivity.this,
-							HamsterActivity.class);
-					Map<String, String> currentItem = (Map<String, String>) parent.getItemAtPosition(position);
-
-					intent.putExtra(TAG_TITLE, currentItem.get(TAG_TITLE));
-					intent.putExtra(TAG_DESCRIPTION,currentItem.get(TAG_DESCRIPTION));
-					intent.putExtra(TAG_IMAGEPATH, currentItem.get(TAG_IMAGEPATH));
-					
-					startActivity(intent);
-
-				}
-
-			});
 		}
 
 	}
