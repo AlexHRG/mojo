fa703bc78a006c9f59e1fb190183ed11b82b66ce
AlexHRG <hirurg_03@ukr.net>
2014-09-29 11:09:01 +0300

    added allocation of pressed element

--- src/com/example/unrealmojo/test/MainActivity.java
+++ src/com/example/unrealmojo/test/MainActivity.java
@@ -51,39 +51,41 @@ public class MainActivity extends Activity {
 
 		listView = (ListView) findViewById(android.R.id.list);
 
-		if (savedInstanceState == null){
+		if (savedInstanceState == null) {
 			new HardTask().execute();
 		} else {
-			item_list = (ArrayList<Map<String, String>>) savedInstanceState.get("items");
+			item_list = (ArrayList<Map<String, String>>) savedInstanceState
+					.get("items");
 		}
-		
+
 		createList();
 	}
-	
-	private void createList(){
-		adapter = new SimpleAdapter(MainActivity.this,
-				item_list, R.layout.list, new String[] { TAG_TITLE,
-						TAG_DESCRIPTION, TAG_IMAGEPATH }, new int[] {
-						R.id.listTitle, R.id.listDescription,
-						R.id.listImage });
-		
+
+	private void createList() {
+		adapter = new SimpleAdapter(MainActivity.this, item_list,
+				R.layout.list, new String[] { TAG_TITLE, TAG_DESCRIPTION,
+						TAG_IMAGEPATH }, new int[] { R.id.listTitle,
+						R.id.listDescription, R.id.listImage });
+
 		listView.setAdapter(adapter);
 		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
 		listView.setOnItemClickListener(new ListView.OnItemClickListener() {
-			
+
 			@Override
-			public void onItemClick(AdapterView<?> parent,
-					View itemClicked, int position, long id) {
+			public void onItemClick(AdapterView<?> parent, View itemClicked,
+					int position, long id) {
 
 				Intent intent = new Intent(MainActivity.this,
 						DetailActivity.class);
-				Map<String, String> currentItem = (Map<String, String>) parent.getItemAtPosition(position);
+				Map<String, String> currentItem = (Map<String, String>) parent
+						.getItemAtPosition(position);
 
 				intent.putExtra(TAG_TITLE, currentItem.get(TAG_TITLE));
-				intent.putExtra(TAG_DESCRIPTION,currentItem.get(TAG_DESCRIPTION));
+				intent.putExtra(TAG_DESCRIPTION,
+						currentItem.get(TAG_DESCRIPTION));
 				intent.putExtra(TAG_IMAGEPATH, currentItem.get(TAG_IMAGEPATH));
-				
+
 				startActivity(intent);
 
 			}
@@ -91,39 +93,40 @@ public class MainActivity extends Activity {
 		});
 
 	}
-	
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	getMenuInflater().inflate(R.menu.main, menu);
-      
-      return super.onCreateOptionsMenu(menu);
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.refresh:
-        	item_list.clear();
-            this.onCreate(null);
-            return true;
-        case R.id.about:
-            showInfo();
-            return true;
-        case R.id.exit:
-            System.exit(0);
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
-    
-	private void showInfo(){
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.refresh:
+			item_list.clear();
+			this.onCreate(null);
+			return true;
+		case R.id.about:
+			showInfo();
+			return true;
+		case R.id.exit:
+			System.exit(0);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void showInfo() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 		String info_line = String.format("%s: %s\n%s: %s\n%s: %s",
 				getResources().getString(R.string.program_title), aboutTitle,
-				getResources().getString(R.string.pogram_version), aboutVersion,
+				getResources().getString(R.string.pogram_version),
+				aboutVersion,
 				getResources().getString(R.string.program_author), aboutAuthor);
-		
+
 		builder.setTitle(R.string.info)
 				.setMessage(info_line)
 				.setCancelable(true)
@@ -136,16 +139,15 @@ public class MainActivity extends Activity {
 		AlertDialog alert = builder.create();
 		alert.show();
 	}
-    
-	protected void onSaveInstanceState(Bundle outState) {
-	    super.onSaveInstanceState(outState);
-	    outState.putSerializable("items", item_list);
-	  }
-	
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-	    super.onRestoreInstanceState(savedInstanceState);
-	  }
 
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putSerializable("items", item_list);
+	}
+
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+	}
 
 	class HardTask extends AsyncTask<Void, Void, Void> {
 
@@ -155,7 +157,7 @@ public class MainActivity extends Activity {
 			pDialog = new ProgressDialog(MainActivity.this);
 			pDialog.setMessage(getResources().getString(R.string.wait));
 			pDialog.setCancelable(true);
-			if (!pDialog.isShowing()){
+			if (!pDialog.isShowing()) {
 				pDialog.show();
 			}
 		}
@@ -174,7 +176,8 @@ public class MainActivity extends Activity {
 
 					String extStorageDirectory = Environment
 							.getExternalStorageDirectory().toString();
-					File filePath = new File(extStorageDirectory + "/" + folderName);
+					File filePath = new File(extStorageDirectory + "/"
+							+ folderName);
 					filePath.mkdir();
 
 					for (int i = 0; i < jsonArray.length(); i++) {
@@ -185,30 +188,31 @@ public class MainActivity extends Activity {
 						String description = null;
 						String imageUrl = null;
 						String imagePath = null;
-						String pinned = null;						
+						String pinned = null;
 
 						title = jsonObject.getString(TAG_TITLE);
 						description = jsonObject.getString(TAG_DESCRIPTION);
 						imageUrl = jsonObject.getString(TAG_IMAGEURL);
-						
+
 						String[] fileNameTokens = imageUrl.split("/");
 						int last = fileNameTokens.length - 1;
 						String fileName = fileNameTokens[last];
-						
+
 						imagePath = ImageDownloader.loadImageToDisc(imageUrl,
-									String.format("%s/%s", filePath.toString(), fileName));
-						
+								String.format("%s/%s", filePath.toString(),
+										fileName));
+
 						pinned = jsonObject.isNull(TAG_PINNED) ? "0" : "1";
-						
+
 						item_data.put(TAG_TITLE, title);
 						item_data.put(TAG_DESCRIPTION, description);
 						item_data.put(TAG_IMAGEPATH, imagePath);
 						item_data.put(TAG_PINNED, pinned);
 
 						item_list.add((HashMap<String, String>) item_data);
-						
+
 						Collections.sort(item_list, new ItemComparator());
-						
+
 					}
 
 				} catch (JSONException e) {
@@ -229,7 +233,7 @@ public class MainActivity extends Activity {
 
 			if (pDialog.isShowing())
 				pDialog.dismiss();
-			
+
 			adapter.notifyDataSetChanged();
 		}
 
