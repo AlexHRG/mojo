0f05c861182eb3285786fe479362c3fa7482be3a
AlexHRG <hirurg_03@ukr.net>
2014-10-22 11:40:31 +0300

    small changes to code

--- src/com/example/unrealmojo/test/ItemComparator.java
+++ src/com/example/unrealmojo/test/ItemComparator.java
@@ -5,17 +5,15 @@ import java.util.Map;
 
 
 public class ItemComparator implements Comparator<Map<String, String>> {
-	private static final String TAG_TITLE = "title";
-	private static final String TAG_PINNED = "pinned";
 
 	@Override
 	public int compare(Map<String, String> left, Map<String, String> right) {
 		if (left == null) return 1;
 		if (right == null) return -1;
-		if (left.get(TAG_PINNED).equals("1") && right.get(TAG_PINNED).equals("0")) return -1;
-		if (left.get(TAG_PINNED).equals("0") && right.get(TAG_PINNED).equals("1")) return 1;
+		if (left.get(MainActivity.TAG_PINNED).equals("1") && right.get(MainActivity.TAG_PINNED).equals("0")) return -1;
+		if (left.get(MainActivity.TAG_PINNED).equals("0") && right.get(MainActivity.TAG_PINNED).equals("1")) return 1;
 
-		return (left.get(TAG_TITLE).toString()).compareTo(right.get(TAG_TITLE).toString());
+		return (left.get(MainActivity.TAG_TITLE).toString()).compareTo(right.get(MainActivity.TAG_TITLE).toString());
 
 	}
 
