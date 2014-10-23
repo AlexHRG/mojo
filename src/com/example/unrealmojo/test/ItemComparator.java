47c419a746cb4ce6757e1c3868973530344b1065
AlexHRG <hirurg_03@ukr.net>
2014-10-23 11:54:48 +0300

    added check for null,null at comparator

--- src/com/example/unrealmojo/test/ItemComparator.java
+++ src/com/example/unrealmojo/test/ItemComparator.java
@@ -8,19 +8,16 @@ public class ItemComparator implements Comparator<Map<String, String>> {
 
 	@Override
 	public int compare(Map<String, String> left, Map<String, String> right) {
+		if (left == right) return 0;
 		if (left == null) return 1;
 		if (right == null) return -1;
-//		if (left.get(MainActivity.TAG_PINNED).equals("1") && right.get(MainActivity.TAG_PINNED).equals("0")) return -1;
-//		if (left.get(MainActivity.TAG_PINNED).equals("0") && right.get(MainActivity.TAG_PINNED).equals("1")) return 1;
-		
-		int leftValue = Integer.valueOf(left.get(MainActivity.TAG_PINNED));
-		int rightValue = Integer.valueOf(right.get(MainActivity.TAG_PINNED));
-		int result = rightValue - leftValue;
+
+		int result = right.get(MainActivity.TAG_PINNED).compareTo(left.get(MainActivity.TAG_PINNED));
 		
 		if (result != 0)
 			return result;
 
-		return (left.get(MainActivity.TAG_TITLE).toString()).compareTo(right.get(MainActivity.TAG_TITLE).toString());
+		return (left.get(MainActivity.TAG_TITLE)).compareTo(right.get(MainActivity.TAG_TITLE));
 
 	}
 
