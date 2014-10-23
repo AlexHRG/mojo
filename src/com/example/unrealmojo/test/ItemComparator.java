078366eaea1ee00bce980ebe960d02e068a12831
AlexHRG <hirurg_03@ukr.net>
2014-10-23 11:06:20 +0300

    optimized comparator for better speed

--- src/com/example/unrealmojo/test/ItemComparator.java
+++ src/com/example/unrealmojo/test/ItemComparator.java
@@ -10,8 +10,15 @@ public class ItemComparator implements Comparator<Map<String, String>> {
 	public int compare(Map<String, String> left, Map<String, String> right) {
 		if (left == null) return 1;
 		if (right == null) return -1;
-		if (left.get(MainActivity.TAG_PINNED).equals("1") && right.get(MainActivity.TAG_PINNED).equals("0")) return -1;
-		if (left.get(MainActivity.TAG_PINNED).equals("0") && right.get(MainActivity.TAG_PINNED).equals("1")) return 1;
+//		if (left.get(MainActivity.TAG_PINNED).equals("1") && right.get(MainActivity.TAG_PINNED).equals("0")) return -1;
+//		if (left.get(MainActivity.TAG_PINNED).equals("0") && right.get(MainActivity.TAG_PINNED).equals("1")) return 1;
+		
+		int leftValue = Integer.valueOf(left.get(MainActivity.TAG_PINNED));
+		int rightValue = Integer.valueOf(right.get(MainActivity.TAG_PINNED));
+		int result = rightValue - leftValue;
+		
+		if (result != 0)
+			return result;
 
 		return (left.get(MainActivity.TAG_TITLE).toString()).compareTo(right.get(MainActivity.TAG_TITLE).toString());
 
